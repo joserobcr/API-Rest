a231fe851cffd2b73dbfa7430f57591a20af68db
joserobcr <joserobcr2003@hotmail.com>
2025-01-30 17:21:06 -0600

    same y cross origin

--- Parcial1/ServidorHTTP-Node/cors.php
+++ Parcial1/ServidorHTTP-Node/cors.php
@@ -1,10 +1,10 @@
 <?php
-// Habilitar CORS para que el frontend pueda hacer peticiones al backend
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Respuesta JSON
+
 $data = ["mensaje" => "Hola, este es un mensaje desde el servidor PHP"];
 echo json_encode($data);
 ?>

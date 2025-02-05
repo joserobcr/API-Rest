d3ea3ca00ac84c9335be50789172071ef87b669d
joserobcr <joserobcr2003@hotmail.com>
2025-02-04 19:18:53 -0600

    cambios

--- Parcial1/ServidorHTTP-Node/cors.php
+++ Parcial1/ServidorHTTP-Node/cors.php
@@ -1,8 +1,8 @@
 <?php
 
-/*header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");*/
+header("Access-Control-Allow-Headers: Content-Type");
 
 
 $data = ["mensaje" => "Hola mundo, este es un mensaje desde el servidor PHP"];

ea86162b9a9bc27c01abf3eedea12fb859cbe0b2
joserobcr <joserobcr2003@hotmail.com>
2025-02-04 21:31:26 -0600

    cambios en index y server

--- Parcial1/Server_Express/index.js
+++ Parcial1/Server_Express/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors'); // Descomenta esta línea
 const app = express();
 const PORT = 3001;
 
-app.get('/',(req, res,next)=>{
-    res.sendFile( __dirname+ '/public/index.html');
+app.use(cors()); // Habilita CORS para todas las rutas
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
-app.post('/',(req, res)=>{
+
+app.post('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
 });
--- Parcial1/ServidorHTTP/Cors/Server.js
+++ Parcial1/ServidorHTTP/Cors/Server.js
@@ -1,11 +1,25 @@
-
 var http = require('http');
 
-let server = http.createServer(function (req,res){
-    res.writeHead(200, {'Content-Type': 'text/plain','Access-Control-Allow-Origin': '*'}); //comentar
+let server = http.createServer(function (req, res) {
+    if (req.method === 'OPTIONS') {
+        // Manejo de preflight request
+        res.writeHead(204, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        });
+        res.end();
+        return;
+    }
+
+    // Respuesta para solicitudes normales
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*' // Permitir todas las solicitudes
+    });
     res.end('Hello World');
-})
+});
 
 server.listen(3000, () => {
-    console.log("Servidor HTTP corriendo en puerto 3000")
-})
+    console.log("Servidor HTTP corriendo en puerto 3000");
+});

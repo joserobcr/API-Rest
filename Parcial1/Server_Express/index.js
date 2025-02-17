2576804a9dc6ad7b87b024eec291d030c6097011
joserobcr <joserobcr2003@hotmail.com>
2025-02-17 09:26:21 -0600

    crud

--- Parcial1/Server_Express/index.js
+++ Parcial1/Server_Express/index.js
@@ -6,6 +6,25 @@ const app = express();
 const path = require('path');
 const PORT = 3001;
 const xmlparser = require('express-xml-bodyparser');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'asesoratec',
+  });
+
+  app.get('/usuarios', (req, res, next) => {
+    console.log(req.query.id);
+    connection.query(
+        'SELECT * FROM `asesoratec.usuarios`',
+        function (err, results, fields) {
+          //console.log(results); // results contains rows returned by server
+          //console.log(fields); // fields contains extra meta data about results, if available
+            res.json({resultado:results});
+        }
+      );
+  });
 
 app.use(cors()); // Habilita CORS para todas las rutas
 

52bdd9747b65d8239efdb2674e33dec703d9f255
joserobcr <joserobcr2003@hotmail.com>
2025-02-21 15:12:48 -0600

    .env listo

--- Parcial1/Server_Express/index.js
+++ Parcial1/Server_Express/index.js
@@ -2,19 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const xmlparser = require('express-xml-bodyparser');
 const http = require('http');
-/*require('dotenv').config();
-console.log('Variables de entorno:', process.env);*/
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, 'VariablesDeEntorno/.env') });
-
 
 let PORT = process.env.PORT;
 
 const app = express();
 const routerUsuario = require('./Router/usuarioRouter.js');
-
-
-console.log('Valor de PORT:', process.env.PORT);
 
 // Middleware para parsear el body de las peticiones
 app.use(express.json());
@@ -31,97 +25,3 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
 });
-
-/*const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'neymar2003',
-    database: 'asesoratec',
-  });*/
-
-  /*app.get('/usuarios', (req, res, next) => {
-    
-    let consulta = '';
-
-    if (typeof req.query.id !== 'undefined') {
-        consulta = `SELECT * FROM usuarios WHERE id = ${req.query.id}`;
-    } else {
-        consulta = `SELECT * FROM usuarios`;
-    }
-    
-    console.log(consulta);
-
-    connection.query(consulta,
-        function (err, results, fields) {
-            console.log(results);
-
-            if(results.length > 0){
-                res.json({resultado:results});
-
-            }else{
-                res.json({resultado: 'No se encontraron resultados'});
-            }
-        }
-      );
-  });*/
-
-/*app.use(cors()); // Habilita CORS para todas las rutas
-
-
-
-//middleware incorporado en express
-app.use('/', (req, res, next) => {
-console.log("Peticion al server")
-next();   
-},
-(req, res, next) => {
-    console.log("2da funcion middleware");
-    next();
-});
-
-const folder = path.join(__dirname + '/ArchivosRecibidos/');
-const upload = multer({ dest: folder });
-app.use(upload.single('archivo'));
-
-//ejemplo thunder client get
-app.get('/alumno', (req, res) => {
-    console.log(req.query);
-    res.sendFile(__dirname + '/public/index.html');;
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-//ejercicio thunder client post
-app.post('/sistemas/:control', (req, res) => {
-    console.log(req.params);
-    res.send('Hola mundo');
-});
-
-//ejercicio thunder client post XML
-app.post('/prefectos', (req, res) => {
-    console.log(req.body);
-    res.send('Hola mundo');
-});
-
-//EJEMPLO THUNDER CLIENT POST CON MULTER
-app.post('/prefectos', (req, res) => {
-    console.log(`Se recibio el archivo : ${req.file.originalname}`);
-    console.log(req.body);
-    console.log('Se recibio el formulario :' +JSON.stringify(req.body));
-    res.json(req.body);
-});
-
-//ejemplo thunder client patch
-app.patch('/maestros', (req, res) => {
-    console.log(req.body);
-    res.send('Hello Worlddddd');
-});
-
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Hola');
-});*/
-
-

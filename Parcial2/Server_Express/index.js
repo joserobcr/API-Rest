2ffdbdb118f8b682f478ddf0a4005ad06c58bc6a
joserobcr <joserobcr2003@hotmail.com>
2025-03-11 09:32:04 -0500

    error log

--- Parcial2/Server_Express/index.js
+++ Parcial2/Server_Express/index.js
@@ -4,11 +4,21 @@ const xmlparser = require('express-xml-bodyparser');
 const http = require('http');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, 'VariablesDeEntorno/.env') });
+const winston = require('winston');
 
 let PORT = process.env.PORT;
 
 const app = express();
 const routerUsuario = require('./Router/usuarioRouter.js');
+const e = require('express');
+
+const logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: __dirname + '/logs/error.log'})
+    ]
+});
 
 // Middleware para parsear el body de las peticiones
 app.use(express.json());
@@ -18,9 +28,10 @@ app.use(cors());
 
 app.use('/usuarios', routerUsuario);
 
-/*app.use((req, res) => {
-    res.status(404).send('404 Not Found');
-});*/
+app.use((err,res,req,next) => {
+    logger.error(err.message, {stack: err.stack});
+    res.status(500).send({error:error.message});
+});
 
 app.use((err, req, res, next) => {
     console.log(err);

371569a1bea9ed27053812b0bbded64d4d8a22a5
joserobcr <joserobcr2003@hotmail.com>
2025-03-14 09:35:13 -0500

    validator

--- Parcial2/Server_Express/Router/usuarioRouter.js
+++ Parcial2/Server_Express/Router/usuarioRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const hal = require('hal');
 const halson = require('halson');
 const usuarioController = require('../Controller/usuarioController.js');
+const { check, validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
     let opciones = {
@@ -12,6 +13,23 @@ router.get('/', (req, res) => {
     res.render('index', opciones); // Renderiza "index.pug" dentro de "views"
 });
 router.get('/', usuarioController.consultarUsuario);
-router.post('/', usuarioController.agregarUsuario); 
+//router.post('/', usuarioController.agregarUsuario); 
+
+router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('correo', 'El correo no es válido').isEmail(),
+        check('contrasena', 'La contraseña debe tener mínimo 6 caracteres').isLength({ min: 6 }),
+        check('rol', 'El rol es obligatorio').not().isEmpty(),
+        check('activo', 'El estado activo/inactivo es obligatorio y debe ser un valor booleano').isBoolean(),
+    ],
+    (req, res, next) => {
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            return res.status(400).json({ errores: errores.array() });
+        }
+        usuarioController.agregarUsuario(req, res, next);
+    }
+);
 
 module.exports = router;
